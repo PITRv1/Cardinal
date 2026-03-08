6019d1fb2024ef188777a9b3326feb85fbf984f7
Mateskio <borokmate@gmail.com>
2026-03-08 16:16:47 +0100

    speed counting fingers

--- MateMagic/MateMagic.cs
+++ MateMagic/MateMagic.cs
@@ -341,7 +341,7 @@ namespace Cardinal
 
                     var node = new NodeBase();
                     node.SetCharacter(c);
-                    node.SetCoords(y, x);
+                    node.SetCoords(x, y);
                     map.WorldMap[y].Add(node);
 
                     if (c == 'S') map.Start = new Point(x, y);
