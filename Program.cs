ee9ebd293ef5ef14a8c0dfd46e3a0182079af88b
PITRv1 <petersandor.toth@gmail.com>
2026-03-08 19:59:45 +0100

    event manager added

--- Program.cs
+++ Program.cs
@@ -8,9 +8,21 @@ sealed class Program
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
+
+
     [STAThread]
     public static void Main(string[] args) {
-        if (args[0] == "-ui") BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        if  (args[0] == "-ui") {
+            Global.ProgramEventManager = new ProgramEventManager();
+
+            //THE PATHFINDING LOGIC NEEDS TO RUN HERE
+            //The resulting file path should be passed into the load function below
+
+            Global.ProgramEventManager.LoadDataFromFile("mission_log.csv");
+
+
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
         else if (args.Length == 1) RoverSolver.Run(new string[] { "mars_map_50x50.csv", args[0] });
     }
 
---
+++ ProgramEventManager.cs
@@ -0,0 +1,92 @@
+﻿using Avalonia.Controls;
+using PETRenderer;
+using Silk.NET.Maths;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cardinal
+{
+    public enum DayPhase {
+        DAY,
+        NIGHT
+    }
+    public enum RoverState
+    {
+        MINING,
+        NAVIGATING,
+        RETURNING
+    }
+
+
+    public struct StepData
+    {
+        public int tick;
+        public float time;
+        public DayPhase phase;
+        public Vector2 position;
+        public float batteryCharge;
+        public int speed;
+        public RoverState state;
+        public int collectedMineralAmount;
+    }
+
+    public class ProgramEventManager
+    {
+        public event Action<StepData> StepDataSent;
+
+        private int numberOfMinerals = 0;
+        private List<StepData> stepDataList = [];
+
+        public void LoadDataFromFile (string dataFile, bool skipFirstLine = true) {
+            List<string> data = File.ReadAllLines(dataFile).ToList<string>();
+            if (skipFirstLine) data.RemoveAt(0);
+
+            numberOfMinerals = int.Parse(data[0].Split(',')[9]);
+
+            foreach (string line in data) {
+                string[] elements = line.Split(',');
+
+                StepData currentStepData = new StepData {
+                    tick = int.Parse(elements[0]),
+                    time = float.Parse(elements[1]),
+                    phase = elements[2] switch {
+                        "Day" => DayPhase.DAY,
+                        "Night" => DayPhase.NIGHT,
+                        _ => throw new Exception($"Unknown day phase: {elements[2]}")
+                    },
+                    position = new Vector2(float.Parse(elements[3]), float.Parse(elements[4])),
+                    batteryCharge = float.Parse(elements[5]),
+                    speed = int.Parse(elements[6]),
+                    state = elements[7] switch {
+                        "Mining" => RoverState.MINING,
+                        "Navigating" => RoverState.NAVIGATING,
+                        "Returning" => RoverState.RETURNING,
+                        _ => throw new Exception($"Unknown rover state: {elements[7]}")
+                    },
+                    collectedMineralAmount = int.Parse(elements[8])
+                };
+            }
+        }
+
+        private StepData GetStepDataAtTick(int tick) {
+            return stepDataList.FirstOrDefault(s => s.tick == tick);
+        }
+
+        public int GetTickCount() {
+            return stepDataList.Count;
+        }
+
+        /// <summary>
+        /// Fires the event loaded with the StepData that happened on the specified tick
+        /// </summary>
+        /// <param name="tick"></param>
+        public void SendUpdateEvent(int tick) {
+            StepDataSent?.Invoke(GetStepDataAtTick(tick));
+        }
+
+    }
+}
