144a5f48b0e126a0c1a62f3d427c309d951ec7e8
scynthi <85178488+scynthi@users.noreply.github.com>
2026-03-16 21:43:35 +0100

    YOO UI

--- Views/RoverTab.axaml.cs
+++ Views/RoverTab.axaml.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
@@ -9,5 +10,30 @@ public partial class RoverTab : UserControl
     public RoverTab()
     {
         InitializeComponent();
+
+        Global.ProgramEventManager.StepDataSent += UpdateUI;
+    }
+
+    private void UpdateUI(StepData stepData)
+    {
+        Avalonia.Threading.Dispatcher.UIThread.InvokeAsync(() =>
+        {
+            BatteryPercentage.Content = $"{stepData.batteryCharge}%";
+            BatteryUsage.Content = stepData.speed;
+            BatteryCharging.Content = stepData.phase == DayPhase.DAY ? "Yes" : "No";
+            BatteryTotalUsed.Content = GetTotalUsageAtTick(stepData.tick).ToString();
+        });
+    }
+
+    private int GetTotalUsageAtTick(int tick)
+    {
+        var prevSteps = Global.ProgramEventManager.stepDataList.Where(s => s.tick < tick);
+        int total = 0;
+
+        if (prevSteps == null) return total;
+
+        foreach (var step in prevSteps) total += 100 - (int)step.batteryCharge;
+
+        return total;
     }
 }
